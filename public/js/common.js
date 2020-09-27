76f4a25f2d9f08ad23987c5385766ac7c471a9b7
luckyone <luckyone1221@gmail.com>
2020-09-27 17:50:39 +0300

    made card links

--- public/js/common.js
+++ public/js/common.js
@@ -305,7 +305,32 @@ function eventHandler() {
 			loadPrevNext: true,
 			loadPrevNextAmount: 5
 		}
-	}); //end luckyoneJs
+	}); //img-svg
+
+	$('img.img-svg-js').each(function () {
+		var $img = $(this);
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+		$.get(imgURL, function (data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = $(data).find('svg'); // Add replaced image's classes to the new SVG
+
+			if (typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass + ' replaced-svg');
+			} // Remove any invalid XML tags as per http://validator.w3.org
+
+
+			$svg = $svg.removeAttr('xmlns:a'); // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+
+			if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+				$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+			} // Replace image with new SVG
+
+
+			$img.replaceWith($svg);
+		}, 'xml');
+	}); // accordion
+	//end luckyoneJs
 }
 
 ;
